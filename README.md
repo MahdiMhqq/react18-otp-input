8d357ca3aa6f75528914e606a5a6d012180a2224
Mahdi Mohaqeq <mahdi.mohaqeq.mm@gmail.com>
2022-07-07 17:36:22 +0430

    initial commit

---
+++ README.md
@@ -0,0 +1,177 @@
+# react18-otp-input
+
+A fully customizable, one-time password input component for the web built with React. This package is based on great work of contributers of `react-otp-input`. But i needed this package and for unknown reasons the contributers of the original package does not accept the open PRs that solve the _REACT 18_ dependency problem. So based on The PR [here](https://github.com/devfolioco/react-otp-input/pull/347) i created this package for the others who need this.
+To see the original package [Click](https://github.com/devfolioco/react-otp-input)
+
+[Live Demo]()
+
+[CodeSandbox]()
+
+#### To install the latest stable version:
+
+```
+npm install --save react18-otp-input
+```
+
+## API
+
+<table>
+  <tr>
+    <th>Name<br/></th>
+    <th>Type</th>
+    <th>Required</th>
+    <th>Default</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td>numInputs</td>
+    <td>number</td>
+    <td>true</td>
+    <td>4</td>
+    <td>Number of OTP inputs to be rendered.</td>
+  </tr>
+  <tr>
+    <td>onChange</td>
+    <td>function</td>
+    <td>true</td>
+    <td>console.log</td>
+    <td>Returns OTP code typed in inputs.</td>
+  </tr>
+  <tr>
+    <td>value</td>
+    <td>string / number</td>
+    <td>true</td>
+    <td>''</td>
+    <td>The value of the OTP passed into the component.</td>
+  </tr>
+    <tr>
+     <td>placeholder</td>
+     <td>string</td>
+     <td>false</td>
+     <td>none</td>
+     <td>Specify an expected value of each input. The length of this string should be equal to <code>numInputs</code>.</td>
+   </tr>
+  <tr>
+    <td>separator</td>
+    <td>component<br/></td>
+    <td>false</td>
+    <td>none</td>
+    <td>Provide a custom separator between inputs by passing a component. For instance, <code>&lt;span&gt;-&lt;/span&gt;</code> would add <code>-</code> between each input.</td>
+  </tr>
+  <tr>
+    <td>containerStyle</td>
+    <td>style (object) / className (string)</td>
+    <td>false</td>
+    <td>none</td>
+    <td>Style applied or class passed to container of inputs.</td>
+  </tr>
+  <tr>
+    <td>inputStyle</td>
+    <td>style (object) / className (string)</td>
+    <td>false</td>
+    <td>none</td>
+    <td>Style applied or class passed to each input.</td>
+  </tr>
+  <tr>
+    <td>focusStyle</td>
+    <td>style (object) / className (string)</td>
+    <td>false</td>
+    <td>none</td>
+    <td>Style applied or class passed to inputs on focus.</td>
+  </tr>
+  <tr>
+    <td>isDisabled</td>
+    <td>boolean</td>
+    <td>false</td>
+    <td>false</td>
+    <td>Disables all the inputs.</td>
+  </tr>
+  <tr>
+    <td>disabledStyle</td>
+    <td>style (object) / className (string)</td>
+    <td>false</td>
+    <td>none</td>
+    <td>Style applied or class passed to each input when disabled.</td>
+  </tr>
+  <tr>
+    <td>hasErrored</td>
+    <td>boolean</td>
+    <td>false</td>
+    <td>false</td>
+    <td>Indicates there is an error in the inputs.</td>
+  </tr>
+  <tr>
+    <td>errorStyle</td>
+    <td>style (object) / className (string)</td>
+    <td>false</td>
+    <td>none</td>
+    <td>Style applied or class passed to each input when errored.</td>
+  </tr>
+  <tr>
+    <td>shouldAutoFocus</td>
+    <td>boolean</td>
+    <td>false</td>
+    <td>false</td>
+    <td>Auto focuses input on initial page load.</td>
+  </tr>
+  <tr>
+    <td>isInputNum</td>
+    <td>boolean</td>
+    <td>false</td>
+    <td>false</td>
+    <td>Restrict input to only numbers.</td>
+  </tr>
+  <tr>
+    <td>isInputSecure</td>
+    <td>boolean</td>
+    <td>false</td>
+    <td>false</td>
+    <td>Masks input characters.</td>
+  </tr>
+  <tr>
+    <td>data-cy</td>
+    <td>string</td>
+    <td>false</td>
+    <td>-</td>
+    <td>Test attribute passed to the inputs.</td>
+  </tr>
+  <tr>
+    <td>data-testid</td>
+    <td>string</td>
+    <td>false</td>
+    <td>-</td>
+    <td>Test attribute passed to the inputs.</td>
+  </tr>
+</table>
+
+## Development
+
+#### To run the development server:
+
+```
+npm run dev
+```
+
+## License
+
+[![NPM](https://img.shields.io/npm/l/react-otp-input)](https://github.com/mahdimhqq/react18-otp-input/blob/master/LICENSE)
+
+## Contributors ✨
+
+Thanks goes to these wonderful people ([emoji key](https://allcontributors.org/docs/en/emoji-key)):
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tr>
+    <td align="center"><a href="https://github.com/MahdiMhqq"><img src="https://avatars.githubusercontent.com/u/90528664?v=4" width="100px;" alt="Mahdi Mohaqeq"/><br /><sub><b>Mahdi Mohaqeq</b></sub></a><br /><a href="https://github.com/mahdimhqq/react18-otp-input/commits?author=mahdimhqq" title="Code">💻</a><a href="#maintenance-mahdimhqq" title="Maintenance">🚧</a> <a href="#ideas-mahdimhqq" title="Ideas, Planning, & Feedback">🤔</a> <a href="" title="Reviewed Pull Requests">👀</a></td>
+  </tr>
+</table>
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+This project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome!
---
+++ src/demo/index.jsx
@@ -0,0 +1,197 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+import OtpInput from '../../lib';
+import './styles.css';
+
+import React from 'react'
+
+function Demo() {
+  const [states, setStates] = useState({
+    otp: '',
+    numInputs: 4,
+    separator: '-',
+    isDisabled: false,
+    hasErrored: false,
+    isInputNum: false,
+    isInputSecure: false,
+    minLength: 0,
+    maxLength: 40,
+    placeholder: '',
+  })
+
+  handleOtpChange = (otp) => {
+    setStates(prev => ({...prev, otp }));
+  };
+
+  handleChange = (e) => {
+    setStates(prev => ({...prev, [e.target.name]: e.target.value }));
+  };
+
+  handleNumInputsChange = (e) => {
+    let numInputs = e.target.value;
+    const { minLength, maxLength } = state;
+
+    if (numInputs < minLength || numInputs > maxLength) {
+      numInputs = 4;
+
+      console.error(`Please enter a value between ${minLength} and ${maxLength}`);
+    }
+
+    setStates(prev => ({...prev, [e.target.name]: parseInt(numInputs, 10) }));
+  };
+
+  clearOtp = () => {
+    setStates(prev => ({...prev, otp: '' }));
+  };
+
+  handleCheck = (e) => {
+    const { name } = e.target;
+    setState(prev => ({...prev, [name]: !prevState[name] }));
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    alert(state.otp);
+  };
+
+  return (
+    <div className="container">
+      <div className="side-bar">
+        <a href="https://github.com/mahdimhqq/react18-otp-input" target="_blank" rel="noreferrer">
+          <div className="side-bar__segment side-bar__segment--header">
+            <h2>react18-otp-input</h2>
+          </div>
+        </a>
+        <div className="side-bar__segment">
+          <label htmlFor="num-inputs">
+            numInputs
+            <input
+              id="num-inputs"
+              name="numInputs"
+              type="number"
+              value={states.numInputs}
+              onChange={handleNumInputsChange}
+              min={states.minLength}
+              max={states.maxLength}
+            />
+          </label>
+        </div>
+        <div className="side-bar__segment">
+          <label htmlFor="separator">
+            separator
+            <input
+              id="separator"
+              maxLength={1}
+              name="separator"
+              type="text"
+              value={states.separator}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div className="side-bar__segment">
+          <label htmlFor="value">
+            value
+            <input id="value" maxLength={states.numInputs} name="otp" type="text" value={states.otp} onChange={handleChange} />
+          </label>
+        </div>
+        <div className="side-bar__segment">
+          <label htmlFor="placeholder">
+            placeholder
+            <input id="placeholder" name="placeholder" type="text" value={states.placeholder} onChange={handleChange} />
+          </label>
+        </div>
+        <div className="side-bar__segment">
+          <label htmlFor="disabled">
+            <input id="disabled" name="isDisabled" type="checkbox" checked={states.isDisabled} onChange={handleCheck} />
+            isDisabled
+          </label>
+        </div>
+        <div className="side-bar__segment">
+          <label htmlFor="hasErrored">
+            <input
+              id="hasErrored"
+              name="hasErrored"
+              type="checkbox"
+              checked={states.hasErrored}
+              onChange={handleCheck}
+            />
+            hasErrored
+          </label>
+        </div>
+        <div className="side-bar__segment">
+          <label htmlFor="isInputNum">
+            <input
+              id="isInputNum"
+              name="isInputNum"
+              type="checkbox"
+              checked={states.isInputNum}
+              onChange={handleCheck}
+            />
+            isInputNum
+          </label>
+        </div>
+        <div className="side-bar__segment">
+          <label htmlFor="isInputSecure">
+            <input
+              id="isInputSecure"
+              name="isInputSecure"
+              type="checkbox"
+              checked={states.isInputSecure}
+              onChange={handleCheck}
+            />
+            isInputSecure
+          </label>
+        </div>
+        <div className="side-bar__segment side-bar__segment--bottom">
+          <a href="https://github.com/mahdimhqq/react18-otp-input">Documentation and Source</a>
+        </div>
+      </div>
+      <div className="view">
+        <div className="card">
+          <form onSubmit={handleSubmit}>
+            <p>Enter verification code</p>
+            <div className="margin-top--small">
+              <OtpInput
+                inputStyle="inputStyle"
+                numInputs={states.numInputs}
+                isDisabled={states.isDisabled}
+                hasErrored={states.hasErrored}
+                errorStyle="error"
+                onChange={handleOtpChange}
+                separator={<span>{states.separator}</span>}
+                isInputNum={states.isInputNum}
+                isInputSecure={states.isInputSecure}
+                shouldAutoFocus
+                value={states.otp}
+                placeholder={states.placeholder}
+              />
+            </div>
+            <div className="btn-row">
+              <button
+                className="btn margin-top--large"
+                type="button"
+                disabled={states.isDisabled || states.otp.trim() === ''}
+                onClick={clearOtp}
+              >
+                Clear
+              </button>
+              <button className="btn margin-top--large" disabled={states.otp.length < states.numInputs}>
+                Get OTP
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const app = ReactDOM.createRoot(document.getElementById('app'));
+app.render(
+  <React.StrictMode>
+    <Demo />
+  </React.StrictMode>
+);
+
